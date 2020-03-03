a003599321fae56ab94e8c73214b5484e8cf7e7c
BinkiZhao <samurai7@foxmail.com>
2020-03-03 20:56:15 +0800

    Updates

---
+++ precache-manifest.8b078fcdccbdf1215ffc6485f83371f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06534fee92aa62be0c1db8cc0eb571ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0477bc87a2e11ce17bc7",
+    "url": "/static/css/main.aad8ebac.chunk.css"
+  },
+  {
+    "revision": "ba73482690cf64fe9717",
+    "url": "/static/js/2.b83b7024.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.b83b7024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0477bc87a2e11ce17bc7",
+    "url": "/static/js/main.5a179060.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
